2588fa3a5aa039ed7baa83e19d205feece6a371c
Mark Laffan <marklaffan4@gmail.com>
2017-12-11 22:06:24 +0000

    add new classes functionality

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
-var express = require("express");
-var app = express();
-
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser");
+    
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-
+var sessions = [
+        {class_name: "Kettlebells", image: "https://cdn.pixabay.com/photo/2017/02/09/16/29/kettlebell-2052775__340.jpg"},
+        {class_name: "Yoga", image: "https://cdn.pixabay.com/photo/2017/08/01/22/44/people-2568410__340.jpg"},
+        {class_name: "TRX", image: "https://cdn.pixabay.com/photo/2016/01/13/22/36/climbing-1139016__340.jpg"},
+        {class_name: "Pilates", image: "https://cdn.pixabay.com/photo/2017/01/03/07/52/weights-1948813__340.jpg"}
+    ];
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/sessions", function(req, res){
-    var sessions = [
-        {class_name: "Kettlebells", trainer: "Brian Byrne", image: "https://cdn.pixabay.com/photo/2017/02/09/16/29/kettlebell-2052775__340.jpg"},
-        {class_name: "Yoga", trainer: "Deberah Kane", image: "https://cdn.pixabay.com/photo/2017/08/01/22/44/people-2568410__340.jpg"},
-        {class_name: "TRX", trainer: "Leo Micheal", image: "https://cdn.pixabay.com/photo/2016/01/13/22/36/climbing-1139016__340.jpg"},
-        {class_name: "Pilates", trainer: "Susan Burnell", image: "https://cdn.pixabay.com/photo/2017/01/03/07/52/weights-1948813__340.jpg"}
-    ];
-
     res.render("sessions", {sessions: sessions});
+});
+
+app.post("/sessions", function(req, res){
+    // Get data from form & add to sessions array
+    var class_name = req.body.class_name;
+    var image = req.body.image;
+    var newSession = {class_name: class_name, image: image};
+    sessions.push(newSession);
+    // Redirect back to classes / sessions page
+    res.redirect("/sessions");
+});
+
+app.get("/sessions/new", function(req, res){
+    res.render("new.ejs");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
