6c2e4f415c8a9e3729ca928a198dd349fbc8e8e8
Mark Laffan <marklaffan4@gmail.com>
2017-12-14 13:54:16 +0000

    Change sessions, class_name to workout/workouts

--- app.js
+++ app.js
@@ -9,36 +9,36 @@ app.set("view engine", "ejs");
 
 // Schema Setup
 var workoutSchema = new mongoose.Schema({
-    class_name: String,
+    workout: String,
     image: String
 });
 
-var sessions = [
-        {class_name: "Kettlebells", image: "https://cdn.pixabay.com/photo/2017/02/09/16/29/kettlebell-2052775__340.jpg"},
-        {class_name: "Yoga", image: "https://cdn.pixabay.com/photo/2017/08/01/22/44/people-2568410__340.jpg"},
-        {class_name: "TRX", image: "https://cdn.pixabay.com/photo/2016/01/13/22/36/climbing-1139016__340.jpg"},
-        {class_name: "Pilates", image: "https://cdn.pixabay.com/photo/2017/01/03/07/52/weights-1948813__340.jpg"}
+var workouts = [
+        {workout: "Kettlebells", image: "https://cdn.pixabay.com/photo/2017/02/09/16/29/kettlebell-2052775__340.jpg"},
+        {workout: "Yoga", image: "https://cdn.pixabay.com/photo/2017/08/01/22/44/people-2568410__340.jpg"},
+        {workout: "TRX", image: "https://cdn.pixabay.com/photo/2016/01/13/22/36/climbing-1139016__340.jpg"},
+        {workout: "Pilates", image: "https://cdn.pixabay.com/photo/2017/01/03/07/52/weights-1948813__340.jpg"}
     ];
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
-app.get("/sessions", function(req, res){
-    res.render("sessions", {sessions: sessions});
+app.get("/workouts", function(req, res){
+    res.render("workouts", {workouts: workouts});
 });
 
-app.post("/sessions", function(req, res){
+app.post("/workouts", function(req, res){
     // Get data from form & add to sessions array
-    var class_name = req.body.class_name;
+    var workout = req.body.workout;
     var image = req.body.image;
-    var newSession = {class_name: class_name, image: image};
-    sessions.push(newSession);
+    var newWorkout = {workout: workout, image: image};
+    workouts.push(newWorkout);
     // Redirect back to classes / sessions page
-    res.redirect("/sessions");
+    res.redirect("/workouts");
 });
 
-app.get("/sessions/new", function(req, res){
+app.get("/workouts/new", function(req, res){
     res.render("new.ejs");
 });
 
