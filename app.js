d132f57167fcbaeaa2bd286ff01e997230fb91e2
Mark Laffan <marklaffan4@gmail.com>
2017-12-17 14:37:49 +0000

    user authentication

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 var express     = require("express"),
     app         = express(),
+    passport    = require("passport"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
+    LocalStrategy   = require("passport-local"),
     Workout     = require("./models/workout"),
     Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    // passportLocalMongoose   = require("passport-local-mongoose"),
     seedDB      = require("./seeds");
 
 
 mongoose.connect("mongodb://localhost/workout_listings", {useMongoClient: true}); // useMongoClient: true removes warning on starting server
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 seedDB();
 
+// Passport Config
+app.use(require("express-session")({
+    secret: "Mark is a Golden God",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
 
 app.get("/", function(req, res){
     res.render("landing");
@@ -24,7 +51,7 @@ app.get("/workouts", function(req, res){
         if(err) {
             console.log(err);
         } else {
-            res.render("workouts/index", {workouts: allWorkouts});
+            res.render("workouts/index", {workouts: allWorkouts, currentUser: req.user});
         }
     });
 });
@@ -71,7 +98,7 @@ app.get("/workouts/:id", function(req, res) {
 // ===============
 
 // New - Show form to create a new comment
-app.get("/workouts/:id/comments/new", function(req, res){
+app.get("/workouts/:id/comments/new", isLoggedIn, function(req, res){
     // Find workout by ID
     Workout.findById(req.params.id, function(err, workout){
         if(err){
@@ -83,7 +110,7 @@ app.get("/workouts/:id/comments/new", function(req, res){
 });
 
 // Create - Add new workout to workouts
-app.post("/workouts/:id/comments", function(req, res){
+app.post("/workouts/:id/comments", isLoggedIn, function(req, res){
     // Lookup workout by ID
     Workout.findById(req.params.id, function(err, workout) {
         if(err){
@@ -105,6 +132,56 @@ app.post("/workouts/:id/comments", function(req, res){
     // Connectcomment to workout
     // Redirect to workout page
 });
+
+// =====================
+// AUTHENTICATION ROUTES
+// =====================
+
+// Show Sign up form
+app.get("/register", function(req, res){
+   res.render("register"); 
+});
+
+// Sign up logic
+app.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/workouts");
+        });
+    });
+});
+
+// Show login form
+app.get("/login", function(req, res){
+   res.render("login"); 
+});
+
+// Login logic
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/workouts",
+        failureRedirect: "/login"
+    }), function(req, res) {
+});
+
+// Logout route
+app.get("/logout", function(req, res){
+   req.logout(); 
+   res.redirect("/workouts");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Workout Listings Server is up and running");
